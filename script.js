c4cd10898f0a27596626b709f54e34d859c486bd
shteratekmt <shteratekmt@gmail.com>
2025-07-07 01:57:10 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,3 @@ function login() {
   alert("Login Google akan dikaitkan ke spreadsheet...");
   window.location.href = "dashboard.html";
 }
-
-function logout() {
-  window.location.href = "index.html";
-}
