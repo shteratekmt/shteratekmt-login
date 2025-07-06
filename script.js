6edbbad6b6312a5b13ca90e40ba911b96487ef16
shteratekmt <shteratekmt@gmail.com>
2025-07-07 02:05:12 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
 
 function login() {
-  const loginButton = document.getElementById("login-btn");
-  loginButton.disabled = true;
-  loginButton.innerText = "Memproses...";
-  loginButton.style.backgroundColor = "#888";
-
-  setTimeout(() => {
-    const loginBerhasil = true;
-
-    if (loginBerhasil) {
-      loginButton.innerText = "Login Berhasil!";
-      loginButton.style.backgroundColor = "#2ecc71";
-      window.location.href = "dashboard.html";
-    } else {
-      loginButton.innerText = "Login Gagal!";
-      loginButton.style.backgroundColor = "#e74c3c";
-      loginButton.disabled = false;
-    }
-  }, 1500);
+  alert("Login Google akan dikaitkan ke spreadsheet...");
+  window.location.href = "dashboard.html";
 }
